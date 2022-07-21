8508b88de1bb155a40cb4781055f5d53f0d868c3
daewon <hannm33@gmail.com>
2022-07-21 15:23:15 +0900

    112323

--- pages/sample/BarChart.vue
+++ pages/sample/BarChart.vue
@@ -19,7 +19,7 @@ import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, Li
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
-    name: 'BarChart',
+    name: 'BarCharttttttt',
     components: { Bar },
     props: {
         chartId: {
